45beb672fa88e18af565394e323f9c0407a45657
MohsenTamtaji <69844938+MohsenTamtaji@users.noreply.github.com>
2022-04-09 07:26:29 +0430

    Update PyEfield.py

--- PyEfield.py
+++ PyEfield.py
@@ -1,56 +1,34 @@
-from numpy import exp, random, dot
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
-import os
 
-r=[[7/100]] #Define the diameter of gold nanospere in nm. For example 7 nm
-Shape=2 # Define the shape of nanoparticle 1 means nanopsphere and 2 means nanocube
+Location=r"E:\Second paper\Efield\FDTDsimulations"# Define the Location of your files
+Shape=1 # Define the shape of nanoparticle 1 stands for nanopsphere and 2 stands for nanocube
+r=[[7],[23]] #Define the diameter of gold nanospere in nm. For example 7 and 20 nm
 
+for i in range(np.size(r)):
+    r[i][0] = (r[i][0]-5)/20
 TrainingNumber=np.size(r)
-class NeuronLayer():
-    def __init__(self, number_of_neurons, number_of_inputs_per_neuron):
-        self.synaptic_weights = 2 * random.random((number_of_inputs_per_neuron, number_of_neurons)) - 1
-class NeuralNetwork():
-    def __sigmoid(self, x):
-        return 1 / (1 + exp(-x))
-
-    def __sigmoid_derivative(self, x):
-        return x * (1 - x)
-
-    def think(self, inputs):
-        output_from_layer1 = self.__sigmoid(dot(inputs, self.layer1.synaptic_weights))
-        output_from_layer2 = self.__sigmoid(dot(output_from_layer1, self.layer2.synaptic_weights))
-        return output_from_layer1, output_from_layer2
-if Shape==1:
-    pickle_in=open("TNNEfield1.pickle","rb")
-elif Shape==2:
-    pickle_in=open("TNNEfield2.pickle","rb")
-else:
-    print('Define the shape of nanoparticle correctly and re-run: 1 stands for nanopsphere and 2 stands for nanocube')
-    os._exit(0)
-neural_network2=pickle.load(pickle_in)
-n, output = neural_network2.think(r)
-meshcellsx=501
-x,y,EE = np.zeros((meshcellsx, meshcellsx)),np.zeros((meshcellsx, meshcellsx)),np.zeros((meshcellsx, meshcellsx))
+loaded_model = pickle.load(open(Location+"\PyEfield"+str(Shape)+".pickle", 'rb'))
+output = loaded_model.predict(r)
+meshcellsx,meshcellsxx=501,250
+x,y,EE = np.zeros((meshcellsxx, meshcellsx)),np.zeros((meshcellsxx, meshcellsx)),np.zeros((meshcellsxx, meshcellsx))
 for TrainingData in range(TrainingNumber):
     jj = 0
-    for i in range(meshcellsx):
+    for i in range(meshcellsxx):
         for j in range(meshcellsx):
-            x[i][j] = (i  - 250) / 250  * 4 * r[TrainingData][0]
-            y[i][j] = (j  - 250) / 250  * 4 * r[TrainingData][0]
+            x[i][j] = (i  - (meshcellsxx-1)) / (meshcellsxx-1) *4 * (r[TrainingData][0]*20+5)
+            y[i][j] = (j  - (meshcellsxx-1)) / (meshcellsxx-1) *4 * (r[TrainingData][0]*20+5)
             EE[i][j] = output[TrainingData][jj]
             jj += 1
-    EE = EE * 10
-    #EE[1][1] = 2.0**1
-    EE[0][0] = 0
     plt.figure()
-    CS=plt.contourf(y, x, EE, 500, cmap='jet',vmin=0,vmax=2)
-    plt.clim(0, 1.5)
+    CS=plt.contourf(y, x, EE, 500, cmap='jet')
+    CS=plt.contourf(y, -x, EE, 500, cmap='jet')
     plt.colorbar(CS)
     plt.xlabel('x (nm)')
     plt.ylabel('y (nm)')
     plt.text(0,0,'Au')
     plt.text(0, 0, 'Au', fontsize=14,fontweight='bold', color='white')
-    plt.text(0, r[TrainingData][0]*2, 'SiO$_2$', fontsize=14,fontweight='bold', color='white')
-    plt.show()
+    plt.text(r[TrainingData][0]*23+5, 0, 'SiO$_2$', fontsize=14,fontweight='bold', color='white')
+    plt.show
+plt.show()
